5f348273985465fe54aed182f31f8a94c06fbff5
Paul356 <panhao356@gmail.com>
2025-04-07 12:18:54 +0800

    Test esp-sr using Rust code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow;
 use esp_idf_svc::hal::{
+    self,
     gpio::{InputPin, OutputPin},
     i2s::{
         config::{
@@ -11,11 +12,28 @@ use esp_idf_svc::hal::{
     peripheral::Peripheral,
     peripherals::{self, Peripherals},
 };
-use esp_idf_svc::sys::esp_sr::{afe_config_init, esp_srmodel_init};
-use esp_idf_svc::sys::{configTICK_RATE_HZ, vTaskDelay};
-use std::ffi::CString;
+use esp_idf_svc::sys;
+use hound::{
+    WavWriter,
+    WavSpec,
+};
+use std::{ffi::CString, os::raw::c_void};
+use std::boxed;
+use sys::esp_sr::{
+    self, afe_config_free, afe_config_init, esp_afe_handle_from_config, esp_mn_commands_add,
+    esp_mn_commands_clear, esp_mn_commands_update, esp_mn_handle_from_name, esp_srmodel_filter,
+    esp_srmodel_init, esp_afe_sr_iface_t, esp_afe_sr_data_t, esp_mn_iface_t, model_iface_data_t
+};
+use sys::{configTICK_RATE_HZ, vTaskDelay};
 
 mod sd_card;
+
+struct FeedTaskArg {
+    afe_handle: *mut esp_sr::esp_afe_sr_iface_t,
+    afe_data: *mut esp_sr::esp_afe_sr_data_t,
+    multinet: *mut esp_sr::esp_mn_iface_t,
+    model_data: *mut esp_sr::model_iface_data_t,
+}
 
 fn init_mic<'d>(
     i2s_slot: impl Peripheral<P = impl I2s> + 'd,
@@ -24,7 +42,7 @@ fn init_mic<'d>(
 ) -> anyhow::Result<I2sDriver<'d, I2sRx>> {
     let pdm_rx_cfg = PdmRxConfig::new(
         Config::default(),
-        PdmRxClkConfig::from_sample_rate_hz(44100)
+        PdmRxClkConfig::from_sample_rate_hz(16000)
             .clk_src(ClockSource::Pll160M)
             .mclk_multiple(MclkMultiple::M256)
             .downsample_mode(PdmDownsample::Samples8),
@@ -39,16 +57,28 @@ fn init_mic<'d>(
     Ok(pdm_driver)
 }
 
-fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+macro_rules! call_c_method {
+    ($c_ptr: expr, $method: ident) => {
+        unsafe {
+            if let Some(inner_func) = (*$c_ptr).$method {
+                Some(inner_func())
+            } else {
+               Err(anyhow::anyhow!("Failed to call method {}", stringify!($method)))
+            }
+        }
+    };
+    ($c_ptr: expr, $method: ident, $($args: expr),*) => {
+        unsafe {
+            if let Some(inner_func) = (*$c_ptr).$method {
+                Ok(inner_func($($args),*))
+            } else {
+                Err(anyhow::anyhow!("Failed to call method {}", stringify!($method)))
+            }
+        }
+    };
+}
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    // let part_name = CString::new("model").unwrap();
-    // let models = unsafe { esp_srmodel_init(part_name.as_ptr()) };
+fn inner_feed_proc(feed_arg: &Box<FeedTaskArg>) -> anyhow::Result<()> {
 
     let peripherals = Peripherals::take()?;
     let mut mic = init_mic(
@@ -57,17 +87,196 @@ fn main() -> anyhow::Result<()> {
         peripherals.pins.gpio41,
     )?;
 
-    let mut sd = sd_card::SdCard::new("/vfat");
-    sd.mount_spi()?;
+    let chunk_size = call_c_method!(feed_arg.afe_handle, get_feed_chunksize, feed_arg.afe_data)?;
 
-    log::info!("Before entering loop");
+    let channel_num = call_c_method!(feed_arg.afe_handle, get_feed_channel_num, feed_arg.afe_data)?;
+
+    log::info!("[INFO] chunk_size {}, channel_num {}", chunk_size, channel_num);
+
+    let mut chunk = vec![0u8; 2 * chunk_size as usize * channel_num as usize];
+
+    //let mut file_idx = 0;
     loop {
-        let mut data = vec![0u8; 1024];
-        let res = mic.read(data.as_mut_slice(), 100)?;
-        log::info!("Read {} bytes from microphone", res);
+        /*
+        let spec = WavSpec {
+            channels: 1,
+            sample_rate: 44100,
+            bits_per_sample: 16,
+            sample_format: hound::SampleFormat::Int,
+        };
 
-        unsafe { vTaskDelay(1 * configTICK_RATE_HZ) };
+        let mut writer = WavWriter::create(std::format!("/vfat/audio{}.wav", file_idx), spec)?;
+
+        let read_cnt = mic.read(chunk.as_mut_slice(), 100)?;
+
+        let mut size = 0;
+        while size < (44100 * 2 * 10) {
+            let read_cnt = mic.read(chunk.as_mut_slice(), 100)?;
+            if read_cnt == 0 {
+                break;
+            }
+            size += read_cnt;
+
+            for i in 0..(read_cnt / 2) {
+                let sample = i16::from_ne_bytes([chunk[i * 2], chunk[i * 2 + 1]]);
+                writer.write_sample(sample)?;
+            }
+        }
+
+        writer.finalize()?;
+        file_idx += 1;
+        */
+
+        mic.read(chunk.as_mut_slice(), 100)?;
+        let _ = call_c_method!(feed_arg.afe_handle, feed, feed_arg.afe_data, chunk.as_ptr() as *const i16)?;
+
     }
+
+    Ok(())
+}
+
+extern "C" fn feed_proc(arg: * mut std::ffi::c_void) {
+    let feed_arg = unsafe { Box::from_raw(arg as *mut FeedTaskArg) };
+
+    inner_feed_proc(&feed_arg).unwrap();
+}
+
+fn inner_fetch_proc(arg: &Box<FeedTaskArg>) -> anyhow::Result<()> {
+    let afe_handle = arg.afe_handle;
+    let afe_data = arg.afe_data;
+    let multinet = arg.multinet;
+    let model_data = arg.model_data;
+
+    let mut detect_flag: bool = false;
+    let mut count: usize = 0;
+    log::info!("Starting detection loop");
+    loop {
+        let res = call_c_method!(afe_handle, fetch, afe_data)?;
+        if res.is_null() || unsafe { (*res).ret_value } == esp_sr::ESP_FAIL {
+            log::error!("Fetch error!");
+            break;
+        }
+
+        if unsafe { (*res).wakeup_state } == esp_sr::wakenet_state_t_WAKENET_DETECTED {
+            log::info!("Wakeword detected");
+            call_c_method!(afe_handle, disable_wakenet, afe_data)?;
+            call_c_method!(multinet, clean, model_data)?;
+            detect_flag = true;
+        } else if detect_flag != true && (count % 10) == 0 {
+            unsafe {
+                log::info!("[INFO] wakeup_state {}, vad_state {}", (*res).wakeup_state, (*res).vad_state);
+            }
+        }
+        count += 1;
+
+        if detect_flag {
+            let mn_state = call_c_method!(multinet, detect, model_data, (*res).data)?;
+            if mn_state == esp_sr::esp_mn_state_t_ESP_MN_STATE_DETECTED {
+                let mn_result = call_c_method!(multinet, get_results, model_data)?;
+                for i in 0..unsafe { (*mn_result).num as usize } {
+                    log::info!(
+                        "Command detected: {}, String: {:?}",
+                        unsafe { (*mn_result).command_id[i] },
+                        unsafe { (*mn_result).string }
+                    );
+                }
+            } else if mn_state == esp_sr::esp_mn_state_t_ESP_MN_STATE_TIMEOUT {
+                log::info!("Timeout, no command detected");
+                call_c_method!(afe_handle, enable_wakenet, afe_data)?;
+                detect_flag = false;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+extern "C" fn fetch_proc(arg: * mut std::ffi::c_void) {
+    let feed_arg = unsafe { Box::from_raw(arg as *mut FeedTaskArg) };
+
+    let res = inner_fetch_proc(&feed_arg);
+}
+
+fn main() -> anyhow::Result<()> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    //let mut sd = sd_card::SdCard::new("/vfat");
+    //sd.mount_spi()?;
+
+    let part_name = CString::new("model").unwrap();
+    let models = unsafe { esp_srmodel_init(part_name.as_ptr()) };
+
+    let input_format = CString::new("M").unwrap();
+    let afe_config = unsafe {
+        afe_config_init(
+            input_format.as_ptr(),
+            models,
+            esp_sr::afe_type_t_AFE_TYPE_SR,
+            esp_sr::afe_mode_t_AFE_MODE_LOW_COST,
+        )
+    };
+
+    let afe_handle = unsafe { esp_afe_handle_from_config(afe_config) };
+    let afe_data = call_c_method!(afe_handle, create_from_config, afe_config)?;
+    unsafe { afe_config_free(afe_config) };
+
+    //let multinet: *mut esp_sr::esp_mn_iface_t = std::ptr::null_mut();
+    //let model_data: *mut esp_sr::model_iface_data_t = std::ptr::null_mut();
+
+    let prefix_str = Vec::from(esp_sr::ESP_MN_PREFIX);
+    let chinese_str = Vec::from(esp_sr::ESP_MN_CHINESE);
+    let mn_name = unsafe {
+        esp_srmodel_filter(
+            models,
+            prefix_str.as_ptr() as *const i8,
+            chinese_str.as_ptr() as *const i8,
+        )
+    };
+
+    let multinet = unsafe { esp_mn_handle_from_name(mn_name) };
+    let model_data = call_c_method!(multinet, create, mn_name, 6000)?;
+
+    unsafe {
+        esp_mn_commands_clear();
+        esp_mn_commands_add(1, Vec::from(b"hao gao xiao\0").as_ptr() as *const i8);
+        esp_mn_commands_add(2, Vec::from(b"ni zhen bang\0").as_ptr() as *const i8);
+        esp_mn_commands_update();
+    }
+
+    let feed_task_arg = Box::new(FeedTaskArg {
+        afe_handle,
+        afe_data,
+        multinet,
+        model_data,
+    });
+
+    let _ = unsafe {
+        hal::task::create(feed_proc, &*CString::new("feed_task").unwrap(), 8 * 1024, Box::into_raw(feed_task_arg) as *mut c_void, 5, None)
+    }?;
+
+    let fetch_task_arg = Box::new(FeedTaskArg {
+        afe_handle,
+        afe_data,
+        multinet,
+        model_data,
+    });
+
+    let _ = unsafe {
+        hal::task::create(fetch_proc, &*CString::new("fetch_task").unwrap(), 8 * 1024, Box::into_raw(fetch_task_arg) as *mut c_void, 5, None)
+    }?;
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
+    // Clean up resources
+    let _ = call_c_method!(multinet, destroy, model_data);
+    let _ = call_c_method!(afe_handle, destroy, afe_data);
 
     Ok(())
 }
