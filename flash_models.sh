b04bd68cd2b9b1ec8c391007b5ad8c8e3ff2e1c8
Paul356 <>
2025-05-10 22:21:42 +0800

    enlarge the image partition

--- flash_models.sh
+++ flash_models.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-esptool.py --chip esp32s3 -p /dev/ttyACM0 -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 80m --flash_size 8MB 0x210000 /home/user1/code/ai-chatbox/target/xtensa-esp32s3-espidf/debug/build/esp-idf-sys-ac*/out/build/srmodels/srmodels.bin
+esptool.py --chip esp32s3 -p /dev/ttyACM0 -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 80m --flash_size 8MB 0x290000 /home/user1/code/ai-chatbox/target/xtensa-esp32s3-espidf/debug/build/esp-idf-sys-ac*/out/build/srmodels/srmodels.bin
